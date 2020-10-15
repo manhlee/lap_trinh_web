6fcb2f4ae58b87ef49a477bf044b649f1de883f7
manhlee <manhdzzd@gmail.com>
2020-10-16 00:14:06 +0700

    add css

--- 1.js
+++ 1.js
@@ -1,4 +1,10 @@
-﻿ $(function(){
-	 
-})  
+﻿ 
+ $(document).ready(function(){
+ 	
+	 $(window).resize(function(){
+	 	var docao = $(window).height();
+	 	$('.big_img').css({'height':docao});
+
+	 });
+ });
  
